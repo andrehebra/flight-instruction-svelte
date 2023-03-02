8e91a3303dd78dae7325f73dc452e36232dc019a
andrehebra <andrehebra@gmail.com>
2023-03-01 22:43:19 -0500

    fixed page.svelt

--- flight-instruction/src/routes/+page.svelte
+++ flight-instruction/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import LessonDisplay from './components/LessonDisplay.svelte';
     import NavBar from './components/NavBar.svelte';
-    export const prerender = true;
 </script>
 
 <NavBar />
