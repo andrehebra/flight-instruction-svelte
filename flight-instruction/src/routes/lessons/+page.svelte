43652804fa07dad3817027d3d5d6666d9cc9f240
andrehebra <andrehebra@gmail.com>
2023-03-02 00:04:04 -0500

    added cards, need to center them

--- flight-instruction/src/routes/lessons/+page.svelte
+++ flight-instruction/src/routes/lessons/+page.svelte
@@ -1,7 +1,41 @@
 <script>
 	import LessonDisplay from "../components/LessonDisplay.svelte";
+	import NavBar from "../components/NavBar.svelte";
+    import LessonCard from "../components/LessonCard.svelte";
+    
+    let lessons = [
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        {title: 'Principles of Flight', sub: 'The basics of lift and fundamentals of how airplanes fly and are controlled.'},
+        
+    ]
 
 </script>
 
-<LessonDisplay />
+<NavBar />
 
+<div class="cardContainer">
+    {#each lessons as lesson}
+        <div class="cardHolder">
+            <LessonCard class='lessonCard' title={lesson.title} sub={lesson.sub}/>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .cardContainer {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .cardHolder {
+        padding: 10px;
+        min-width: 300px;
+    }
+</style>
