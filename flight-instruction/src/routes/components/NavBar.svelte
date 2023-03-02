1b88e5f1468e7858e2a1f4a42a74c95b110b0533
andrehebra <andrehebra@gmail.com>
2023-03-01 19:03:57 -0500

    added navbar and its own component

---
+++ flight-instruction/src/routes/components/NavBar.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+</script>
+
+<Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+      <img
+        src="https://flowbite.com/docs/images/logo.svg"
+        class="mr-3 h-6 sm:h-9"
+        alt="Logo"
+      />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        Flight Instruction
+      </span>
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+      <NavLi href="/" active={true}>Home</NavLi>
+      <NavLi href="/lessons">Lessons</NavLi>
+    </NavUl>
+  </Navbar>
