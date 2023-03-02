bd647059169464911e3ad7cb33243d576344945e
andrehebra <andrehebra@gmail.com>
2023-03-02 02:18:49 -0500

    got sidebar linking to work correctly

---
+++ flight-instruction/src/routes/components/LessonConstructor.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {  Heading, P, A, Mark, Secondary } from 'flowbite-svelte'
+
+    export let contents;
+</script>
+
+<h1>{contents[0].text}</h1>
+{#each contents as widget}
+    {#if widget.widget == 'header'}
+        <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">{widget.text}</Heading>
+    {:else if widget.widget == 'text'}
+        <P size="base">{widget.text}</P>
+    {/if}
+{/each}
--- flight-instruction/src/routes/components/LessonDisplay.svelte
+++ flight-instruction/src/routes/components/LessonDisplay.svelte
@@ -9,15 +9,20 @@
 	for (let i = 0; i < contents.length; i++) {
 		sidebarItems.push({title: contents[i].title, id: i});
 
-		pages.push({id: i, contents: contents[i].contents});
+		pages.push(contents[i].contents);
 	}
+	let children;
 
     import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, SidebarDropdownItem, SidebarDropdownWrapper } from 'flowbite-svelte';
 	import LessonConstructor from "./LessonConstructor.svelte";
     let spanClass = 'flex-1 ml-3 whitespace-nowrap';
 
+	let unique = {};
+	children = pages[0];
 	function openPage(id) {
-		alert('no more alerts');
+		children = pages[id];
+		console.log(pages[id]);
+		unique = {};
 	}
 </script>
 
@@ -26,11 +31,14 @@
     <SidebarWrapper>
       <SidebarGroup>
 			{#each sidebarItems as item}
-				<SidebarItem on:click={openPage(item.id)} label={item.title}>
+				
+				<SidebarItem on:click={()=>{openPage(item.id)}} label={item.title}>
 				</SidebarItem>
 			{/each}
       	</SidebarGroup>
     </SidebarWrapper>
 </Sidebar>
 
-<LessonConstructor />
+{#key unique}
+<LessonConstructor contents={children} />
+{/key}
