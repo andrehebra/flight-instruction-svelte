e8b161ceacb1219f3efd046f390b5cce93266663
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-03-02 19:10:57 -0500

    added dropdown functionality

--- flight-instruction/src/routes/components/LessonDisplay.svelte
+++ flight-instruction/src/routes/components/LessonDisplay.svelte
@@ -3,18 +3,34 @@
 
     export let contents;
 	let sidebarItems = [];
+	let dropDownItems = [];
 
 	let pages = [];
 
+	let counter = 0;
 	for (let i = 0; i < contents.length; i++) {
-		if (i == 0) {
-			sidebarItems.push({title: contents[i].title, id: i, active: true});
+		if (contents[i].drop == true) {
+			dropDownItems = [];
+			for (let j = 0; j < contents[i].contents.length; j++) {
+				dropDownItems.push({title: contents[i].contents[j].title, id: counter, active: false, contents: contents[i].contents[j].contents});
+				pages.push(contents[i].contents[j].contents);
+				counter++;
+			}
+			sidebarItems.push({title: contents[i].title, id: counter, active: false, drop: true, contents: dropDownItems});
 		} else {
-			sidebarItems.push({title: contents[i].title, id: i, active: false});
+			if (i == 0) {
+				sidebarItems.push({title: contents[i].title, id: counter, active: true, drop: false});
+			} else {
+				sidebarItems.push({title: contents[i].title, id: counter, active: false, drop: false});
+			}
+			pages.push(contents[i].contents);
 		}
+
+		counter++;
 		
-		pages.push(contents[i].contents);
 	}
+	console.log(pages);
+	console.log(sidebarItems);
 
 	let children;
 
@@ -26,13 +42,35 @@
 	children = pages[0];
 	function openPage(id) {
 		sidebarItems = [];
+		counter = 0;
+		let active = false;
 		for (let i = 0; i < contents.length; i++) {
-			if (i == id) {
-				sidebarItems.push({title: contents[i].title, id: i, active: true});
+			
+			if (contents[i].drop == true) {
+				dropDownItems = [];
+				for (let j = 0; j < contents[i].contents.length; j++) {
+					if (counter == id) {
+						active = true;
+					}
+					dropDownItems.push({title: contents[i].contents[j].title, id: counter, active: active, contents: contents[i].contents[j].contents});
+					counter++;
+					active = false;
+				}
+				sidebarItems.push({title: contents[i].title, id: counter, active: active, drop: true, contents: dropDownItems});
 			} else {
-				sidebarItems.push({title: contents[i].title, id: i, active: false});
+				if (counter == id) {
+					active = true;
+				}
+				if (i == 0) {
+					sidebarItems.push({title: contents[i].title, id: counter, active: active, drop: false});
+				} else {
+					sidebarItems.push({title: contents[i].title, id: counter, active: active, drop: false});
+				}
 			}
-		}
+		active = false;
+		counter++;
+	}
+
 		children = pages[id];
 		console.log(pages[id]);
 		unique = {};
@@ -45,8 +83,18 @@
 		<SidebarWrapper>
 		  <SidebarGroup>
 				{#each sidebarItems as item}
+					{#if item.drop == false}
 					<SidebarItem active={item.active} on:click={()=>{openPage(item.id)}} label={item.title}>
 					</SidebarItem>
+					{:else if item.drop == true}
+						<SidebarDropdownWrapper label={item.title}>
+							{#each item.contents as content}
+								<SidebarItem active={content.active} on:click={()=>{openPage(content.id)}} label={content.title}>
+								</SidebarItem>
+							{/each}
+						</SidebarDropdownWrapper>
+					{/if}
+					
 				{/each}
 			  </SidebarGroup>
 		</SidebarWrapper>
