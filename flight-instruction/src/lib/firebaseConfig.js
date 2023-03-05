5cbe319f779c21c2856ae6a3c91b2220e6cc6ff8
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-03-05 16:14:04 -0500

    installed firebase

---
+++ flight-instruction/src/lib/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCII7xK01uOodP9uzZul7583HXj22tPi8Q",
+    authDomain: "flight-instruction-ffdb7.firebaseapp.com",
+    projectId: "flight-instruction-ffdb7",
+    storageBucket: "flight-instruction-ffdb7.appspot.com",
+    messagingSenderId: "950508746933",
+    appId: "1:950508746933:web:28e135744861f236ce26fc",
+    measurementId: "G-7388X41K0M"
+};
