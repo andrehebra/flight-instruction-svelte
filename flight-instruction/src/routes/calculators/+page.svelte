0ffa32ebb7ff6d82b1b7839b68b0ea01002e66b1
andrehebra <andrehebra@gmail.com>
2023-03-27 13:15:18 -0400

    added calculator page

---
+++ flight-instruction/src/routes/calculators/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import NavBar from "../components/NavBar.svelte";
+    import {  Heading, P, A, Mark, Secondary } from 'flowbite-svelte';
+    import { Input, Label, Helper } from 'flowbite-svelte';
+
+</script>
+
+<NavBar />
+<Heading>Rate of turn calculator</Heading>
+<div>
+    <Label for="first_name" class="mb-2">First name</Label>
+    <Input type="text" id="first_name" placeholder="John" required  />
+</div>
