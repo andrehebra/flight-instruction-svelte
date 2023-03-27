8bc9fe7cc18df6125a1bd98940745286703159f1
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-03-27 14:13:25 -0400

    pressure/density calculator working, need to style

--- flight-instruction/src/routes/calculators/pressureanddensityaltitude/+page.svelte
+++ flight-instruction/src/routes/calculators/pressureanddensityaltitude/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import NavBar from "../../components/NavBar.svelte";
-    import { FloatingLabelInput } from "flowbite-svelte";
+    import { FloatingLabelInput, Heading } from "flowbite-svelte";
 
     let indicated = 0;
     let pressure = 29.92;
@@ -8,17 +8,28 @@
     let pressureAlt;
     let densityAlt
 
-    function calculate() {
-        pressureAlt = (pressure - 29.92) * 1000 + indicated;
-        densityAlt = pressureAlt + (120 * (temperature - 15));
-    }
+    $: pressureAlt = parseInt((pressure - 29.92) * 1000 + indicated);
+    $: densityAlt = parseInt(pressureAlt + (120 * (temperature - 15)));
     
 </script>
 
 <NavBar />
 
-<FloatingLabelInput style="outlined" on:change={() => calculate()} id="floating_outlined" name="floating_outlined" type="text" label="Indicated Altitude (ft)" bind:value={indicated} />
-<FloatingLabelInput placeholder="" on:change={() => calculate()} style="outlined" id="floating_outlined" name="floating_outlined" type="text" label="Pressure (InHg)" bind:value={pressure} />
-<FloatingLabelInput style="outlined" on:change={() => calculate()} id="floating_outlined" name="floating_outlined" type="text" label="Temperature (C)" bind:value={temperature} />
-<h1>{pressureAlt}</h1>
-<h1>{densityAlt}</h1>
+<div class="holder">
+    <FloatingLabelInput style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Indicated Altitude (ft)" bind:value={indicated} />
+    <FloatingLabelInput placeholder=""  style="outlined" id="floating_outlined" name="floating_outlined" type="text" label="Pressure (InHg)" bind:value={pressure} />
+    <FloatingLabelInput style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Temperature (C)" bind:value={temperature} />
+    <Heading tag="h2" customSize="text-4xl font-extrabold ">Pressure Altitude: {pressureAlt}</Heading>
+    <Heading tag="h2" customSize="text-4xl font-extrabold ">Density Altitude: {densityAlt}</Heading>
+</div>
+
+<style>
+    .holder {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        max-width: 500px;
+        gap: 15px;
+        
+    }
+</style>
