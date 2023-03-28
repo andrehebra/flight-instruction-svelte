ef5d3ef7aa53b7dfec88e17255b9f0e8d755a6fe
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-03-28 17:07:04 -0400

    pivotal altitude completed

---
+++ flight-instruction/src/routes/calculators/pivotalaltitude/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import CalculatorDisplay from "../../components/calculatorDisplay.svelte";
+    import { Heading, FloatingLabelInput } from "flowbite-svelte";
+
+    let groundSpeed;
+    let elevation = 0;
+    let pivotalAltitude, pivotalAGL;
+    $: pivotalAltitude = Math.round((groundSpeed * groundSpeed) / 11.3);
+    $: pivotalAGL = pivotalAltitude + parseInt(elevation);
+</script>
+
+<CalculatorDisplay>
+    <Heading>Pivotal Altitude Calculator</Heading>
+    <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Groundspeed (Kts)" bind:value={groundSpeed} />
+    <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Ground Elevation (feet)" bind:value={elevation} />
+
+    <Heading>Pivotal Altitude (AGL): {pivotalAltitude}</Heading>
+    <Heading>Pivotal Altitude (MSL): {pivotalAGL}</Heading>
+</CalculatorDisplay>
---
+++ flight-instruction/src/routes/components/calculatorDisplay.svelte
@@ -0,0 +1,20 @@
+<script>
+    import NavBar from "./NavBar.svelte"
+</script>
+
+<NavBar></NavBar>
+
+<div class="display">
+    <slot></slot>
+</div>
+
+<style>
+    .display {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        max-width: 800px;
+        gap: 15px;
+        
+    }
+</style>
