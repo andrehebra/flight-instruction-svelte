9eadf76fc626bf437f54d5b227bb40b0ee2a395e
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-03-28 17:52:45 -0400

    completed cloud base and freezing level calculator

--- flight-instruction/src/routes/calculators/cloudbaseandfreezinglevel/+page.svelte
+++ flight-instruction/src/routes/calculators/cloudbaseandfreezinglevel/+page.svelte
@@ -9,6 +9,11 @@
     let tempSpread;
 
     $: tempSpread = temperature - dewpoint;
+    $: cloudbaseAGL = Math.round(1000 * tempSpread / 2.44);
+    $: cloudbaseMSL = Math.round(parseInt(cloudbaseAGL) + (Number.isInteger(parseInt(indicatedAlt)) ? parseInt(indicatedAlt) : 0));
+
+    $: freezinglevelAGL = Math.round((temperature) / 2.44 * 1000);
+    $: freezinglevelMSL = Math.round(parseInt(freezinglevelAGL) + (Number.isInteger(parseInt(indicatedAlt)) ? parseInt(indicatedAlt) : 0));
 
 
 
@@ -21,6 +26,8 @@
     <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Dew Point (C)" bind:value={dewpoint} />
     <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Indicated Altitude (feet)" bind:value={indicatedAlt} />
 
-    <Heading>Cloud Base (MSL): {cloudbaseMSL}</Heading>
-    <Heading>Freezing Level (MSL): {freezinglevelMSL}</Heading>
+    <Heading tag="h2" customSize="text-4xl font-extrabold ">Cloud Base (MSL): {cloudbaseMSL}</Heading>
+    <Heading tag="h2" customSize="text-4xl font-extrabold ">Freezing Level (MSL): {freezinglevelMSL}</Heading>
+    <Heading tag="h2" customSize="text-4xl font-extrabold ">Cloud Base (AGL): {cloudbaseAGL}</Heading>
+    <Heading tag="h2" customSize="text-4xl font-extrabold ">Freezing Level (AGL): {freezinglevelAGL}</Heading>
 </CalculatorDisplay>
