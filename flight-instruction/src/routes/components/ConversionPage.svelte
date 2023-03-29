12d5751e395afa44a466ef86f66be630617cf516
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-03-28 21:04:42 -0400

    only one way conversion working

--- flight-instruction/src/routes/components/ConversionPage.svelte
+++ flight-instruction/src/routes/components/ConversionPage.svelte
@@ -8,12 +8,27 @@
     let firstval = 0, secondval;
 
     const { convert } = getContext('convert');
+    let currentChange = 1;
+
+    function changeCurrent(number) {
+        if (number == 1) {
+            currentChange = 1;
+        } else if (number == 2) {
+            currentChange = 2;
+        }
+        console.log(currentChange);
+    }
 
     function handleConversion(place) {
         if (place == 1) {
-            secondval = Math.round(convert(firstval, 1) * 100) / 100;
+            if (currentChange == 1) {
+                secondval = Math.round(convert(firstval, 1) * 100) / 100;
+            }
+            
         } else if (place == 2) {
-            firstval = Math.round(convert(secondval, 2) * 100) / 100;
+            if (currentChange == 2) {
+                firstval = Math.round(convert(secondval, 2) * 100) / 100;
+            }
         }
     }
 
@@ -28,9 +43,9 @@
 <CalculatorDisplay>
     <Heading>Convert {first} and {second}</Heading>
     <Heading tag="h2" customSize="text-4xl font-extrabold ">{first}</Heading>
-    <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" bind:value={firstval} />
+    <FloatingLabelInput on:input={() => changeCurrent(1)} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" bind:value={firstval} />
 
     <Heading tag="h2" customSize="text-4xl font-extrabold ">{second}</Heading>
-    <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" bind:value={secondval} />
+    <FloatingLabelInput on:input={() => changeCurrent(2)} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" bind:value={secondval} />
 
 </CalculatorDisplay>
