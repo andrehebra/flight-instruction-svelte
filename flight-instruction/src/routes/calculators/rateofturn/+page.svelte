69d0d83adfcda7fd41a0d87c5d8219a2e01b4161
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-03-29 17:22:11 -0400

    changed font size

--- flight-instruction/src/routes/calculators/rateofturn/+page.svelte
+++ flight-instruction/src/routes/calculators/rateofturn/+page.svelte
@@ -25,15 +25,15 @@
     <Heading>Bank Angle for Standard Rate Turn Calculator</Heading>
     <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="True Airspeed (Kts)" bind:value={tas} />
 
-    <Heading tag="h2" customSize="text-4xl font-extrabold ">Bank angle for standard rate turn: {standardRate}</Heading>
-    <Heading tag="h2" customSize="text-4xl font-extrabold ">Bank angle for 1/2 standard rate turn: {halfStandardRate}</Heading>
+    <Heading tag="h3">Bank angle for standard rate turn: {standardRate}</Heading>
+    <Heading tag="h3">Bank angle for 1/2 standard rate turn: {halfStandardRate}</Heading>
 
     <Hr class="my-8" height="h-px" />
 
     <Heading>Rate of Turn From Bank Angle</Heading>
     <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="True Airspeed (Kts)" bind:value={tas} />
     <FloatingLabelInput placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Bank Angle" bind:value={bankAngle} />
-    <Heading tag="h2" customSize="text-4xl font-extrabold ">Rate of Turn (deg/sec): {(Number.isNaN(calculatedRate)) ? 0 : calculatedRate}</Heading>
+    <Heading tag="h3">Rate of Turn (deg/sec): {(Number.isNaN(calculatedRate)) ? 0 : calculatedRate}</Heading>
 
 </CalculatorDisplay>
 
