542287a0741159ba03a685c4fe0163a9d690c878
andrehebra <andrehebra@gmail.com>
2023-03-29 14:53:05 -0400

    edited layout for landing page

--- flight-instruction/src/routes/calculators/landingdistance/+page.svelte
+++ flight-instruction/src/routes/calculators/landingdistance/+page.svelte
@@ -206,51 +206,54 @@
 
 <NavBar />
 
-<div class="holder">
-    <Heading>Cessna 172S Short Field Landing Distance Calculator</Heading>
-    <FloatingLabelInput on:change={() => calculatePressureDistance()} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Pressure Altitude" bind:value={pressureAlt} />
-    <FloatingLabelInput on:change={() => calculatePressureDistance()} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Temperature" bind:value={temperature} />
-    <FloatingLabelInput on:change={() => calculatePressureDistance()} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Headwind (enter negative for tailwind)" bind:value={headwind} />
-
-    <div class="radio">
-        <p class="mb-4 font-semibold text-gray-900 dark:text-white">Runway Surface</p>
-        <ul class="items-center w-full rounded-lg border border-gray-200 sm:flex dark:bg-gray-800 dark:border-gray-600 divide-x divide-gray-200 dark:divide-gray-600">
-            <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={selected} value="paved" name="runway-list" class="p-3">Paved</Radio></li>
-            <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={selected} value="grass" name="runway-list" class="p-3">Dry Grass</Radio></li>
-        </ul>
-    </div>
-
-    <div class="radio">
-        <p class="mb-4 font-semibold text-gray-900 dark:text-white">Flaps</p>
-        <ul class="items-center w-full rounded-lg border border-gray-200 sm:flex dark:bg-gray-800 dark:border-gray-600 divide-x divide-gray-200 dark:divide-gray-600">
-            <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={flaps} value="extended" name="flap-list" class="p-3">Extended</Radio></li>
-            <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={flaps} value="retracted" name="flap-list" class="p-3">Retracted</Radio></li>
-        </ul>
-    </div>
+<div class="display">
+    <div class="holder">
+        <Heading>Cessna 172S Short Field Landing Distance Calculator</Heading>
+        <FloatingLabelInput on:change={() => calculatePressureDistance()} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Pressure Altitude" bind:value={pressureAlt} />
+        <FloatingLabelInput on:change={() => calculatePressureDistance()} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Temperature" bind:value={temperature} />
+        <FloatingLabelInput on:change={() => calculatePressureDistance()} placeholder="" style="outlined"  id="floating_outlined" name="floating_outlined" type="text" label="Headwind (enter negative for tailwind)" bind:value={headwind} />
+    
+        <div class="radio">
+            <p class="mb-4 font-semibold text-gray-900 dark:text-white">Runway Surface</p>
+            <ul class="items-center w-full rounded-lg border border-gray-200 sm:flex dark:bg-gray-800 dark:border-gray-600 divide-x divide-gray-200 dark:divide-gray-600">
+                <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={selected} value="paved" name="runway-list" class="p-3">Paved</Radio></li>
+                <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={selected} value="grass" name="runway-list" class="p-3">Dry Grass</Radio></li>
+            </ul>
+        </div>
+    
+        <div class="radio">
+            <p class="mb-4 font-semibold text-gray-900 dark:text-white">Flaps</p>
+            <ul class="items-center w-full rounded-lg border border-gray-200 sm:flex dark:bg-gray-800 dark:border-gray-600 divide-x divide-gray-200 dark:divide-gray-600">
+                <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={flaps} value="extended" name="flap-list" class="p-3">Extended</Radio></li>
+                <li class="w-full"><Radio on:change={() => calculatePressureDistance()} bind:group={flaps} value="retracted" name="flap-list" class="p-3">Retracted</Radio></li>
+            </ul>
+        </div>
+            
         
+        <Heading tag="h2" customSize="text-4xl font-extrabold ">Ground Roll: {Math.ceil(landingDistance)} feet</Heading>
+        <Heading tag="h2" customSize="text-4xl font-extrabold ">50 Foot Obstacle Distance: {Math.ceil(clearanceDistance)} feet</Heading>
     
-    <Heading tag="h2" customSize="text-4xl font-extrabold ">Ground Roll: {Math.ceil(landingDistance)} feet</Heading>
-    <Heading tag="h2" customSize="text-4xl font-extrabold ">50 Foot Obstacle Distance: {Math.ceil(clearanceDistance)} feet</Heading>
-
-    <Hr class="my-8" height="h-px" />
-
-    {#key unique}
-        <Heading tag="h2" customSize="text-4xl font-extrabold ">Takeoff Distance Numbers, (data used shown in red)</Heading>
-        <Heading tag="h3" customSize="text-4xl font-extrabold ">Ground Roll</Heading>
-        <TableBuilder contents={distanceValues} index=0></TableBuilder>
-        <Heading tag="h3" customSize="text-4xl font-extrabold ">Total Distance Over 50 Foot Obstacle</Heading>
-        <TableBuilder contents={distanceValues} index=1></TableBuilder>
-    {/key}
-
-    <Hr class="my-8" height="h-px" />
+        <Hr class="my-8" height="h-px" />
     
-    <Heading tag="h2" customSize="text-4xl font-extrabold ">Explanation of Calculation</Heading>
-    <P>This calculator uses data from the Cessna 172S POH, which assumes a weight of 2550 pounds, flaps set at 30 degrees, power off, maximum braking used, a paved, level, and dry runway, zero wind, and a speed of 61 KIAS at 50 feet.</P>
-    <P>First, data is interpolated from the temperature and pressure numbers given.</P>
-    <P>Next, headwind and tailwind is used to modify the previous calculations. For this calculator, distances are decreased by 10% for each 9 knots of headwind, and distances are increased by 10% for each 2 knots of tailwind.</P>
-    <P>If a grass runway is selected, the ground roll is increased by 45%, and that increase is then added into the total value.</P>
-    <P>Finally, the distances are increased by 35% if the option of flaps retracted is selected.</P>
+        {#key unique}
+            <Heading tag="h2" customSize="text-4xl font-extrabold ">Takeoff Distance Numbers, (data used shown in red)</Heading>
+            <Heading tag="h3" customSize="text-4xl font-extrabold ">Ground Roll</Heading>
+            <TableBuilder contents={distanceValues} index=0></TableBuilder>
+            <Heading tag="h3" customSize="text-4xl font-extrabold ">Total Distance Over 50 Foot Obstacle</Heading>
+            <TableBuilder contents={distanceValues} index=1></TableBuilder>
+        {/key}
+    
+        <Hr class="my-8" height="h-px" />
+        
+        <Heading tag="h2" customSize="text-4xl font-extrabold ">Explanation of Calculation</Heading>
+        <P>This calculator uses data from the Cessna 172S POH, which assumes a weight of 2550 pounds, flaps set at 30 degrees, power off, maximum braking used, a paved, level, and dry runway, zero wind, and a speed of 61 KIAS at 50 feet.</P>
+        <P>First, data is interpolated from the temperature and pressure numbers given.</P>
+        <P>Next, headwind and tailwind is used to modify the previous calculations. For this calculator, distances are decreased by 10% for each 9 knots of headwind, and distances are increased by 10% for each 2 knots of tailwind.</P>
+        <P>If a grass runway is selected, the ground roll is increased by 45%, and that increase is then added into the total value.</P>
+        <P>Finally, the distances are increased by 35% if the option of flaps retracted is selected.</P>
+    </div>
 </div>
+
 
 <style>
     .holder {
@@ -259,6 +262,10 @@
         flex-direction: column;
         max-width: 800px;
         gap: 15px;
-        
+    }
+    .display {
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 </style>
