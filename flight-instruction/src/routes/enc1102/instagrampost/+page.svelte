3448e915469d604e7098a40f1245d431cbacf95f
andrehebra <andrehebra@gmail.com>
2023-04-19 11:51:06 -0400

    added instagram post

---
+++ flight-instruction/src/routes/enc1102/instagrampost/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import NavBar from "../../components/NavBar.svelte";
+    import FirestoreLessonConstructor from '../../components/FirestoreLessonConstructor.svelte';
+
+    let contents = [['image', 'https://i.imgur.com/7A8Fp66.png'],];
+</script>
+
+<FirestoreLessonConstructor contents={contents}></FirestoreLessonConstructor>
