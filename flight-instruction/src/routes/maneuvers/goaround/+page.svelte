97e8ecd5b6d6f886828c1f53b92b29fe1a3c4327
andrehebra <andrehebra@gmail.com>
2023-04-21 13:24:03 -0400

    working on go arounds

--- flight-instruction/src/routes/maneuvers/goaround/+page.svelte
+++ flight-instruction/src/routes/maneuvers/goaround/+page.svelte
@@ -8,8 +8,32 @@
 
 <div class="holder"><div class="contents">
 
-<Heading tag='h1'>Go Arounds</Heading>
+<Heading tag='h1'>Go-Arounds</Heading>
 <P>Learning how to abort a landing is arguably the most important step in learning how to land safely. While it may seem like a simple task, there are many mistakes that pilots can make that can lead to potentially dangerous situations. This lesson will focus on when to do a go around and common mistakes that people make when attempting them.</P>
+<Heading tag='h2'>When to Execute a Go-Around</Heading>
+<P>A go-around should be executed whenever any approach and landing parameters deviate from expectations or when it is harzardous to continue. A few common reasons you may need to go around are below:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Avoiding a conflict on a runway (could be other traffic or any obstruction)</Li>
+<Li>Not properly aligned with the runway centerline</Li>
+<Li>Wind shear on final</Li>
+<Li>Being too fast or too slow</Li>
+<Li>Being too high or too low</Li>
+<Li>When something doesn't feel right</Li></List>
+<P>Think about it like this: expect to go around, landing is just a bonus. Your flight instructor will never be mad at you for going around, even if there was nothing wrong with your approach. It's always better to be extra cautious. There is no risk in going around, assuming you know how to properly execute one, but there is a lot of risk in continuing an approach that you don't feel comfortable with.</P>
+<Heading tag='h2'>Don't Delay When Something is Off</Heading>
+<P>There are many reasons why people continue bad landings, however they often happen from two reasons: landing expectancy and pride.</P>
+<Heading tag='h5'>Landing Expectancy</Heading>
+<P>The belief that conditions are not as threatening as they are and the approach is sure to terminate with a safe landing. This false assumption can affect all pilots, regardless of their experience level. Remember, always take the safe choice, and never underestimate hazards you are facing.</P>
+<Heading tag='h5'>Pride</Heading>
+<P>Going around is not a failure, in fact it is a good measure of aeronautical decision making. Maybe you're worried about impressing your DPE, or just a passenger. Trust me, neither of them are going to be impressed with you continuing a bad landing and ripping off one of the landing gear. Turning a bad approach into an even worse landing is never a good idea.</P>
+<Heading tag='h2'>Executing a Go-Around</Heading>
+<P>A proper go-around includes three principles, in this order:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Power</Li>
+<Li>Attitude</Li>
+<Li>Configuration</Li></List>
+<Heading tag='h5'>Power</Heading>
+<P>This should be your first concern. As soon as you decide to go around, apply full power. You need to overcome the inertia of the airplane's descent and increase your airspeed. Remember that the application of power should be smooth, so don't just slam the throttle forward. </P>
+<Heading tag='h5'>Attitude</Heading>
+<Heading tag='h5'>Configuration</Heading>
 <Heading tag='h2'>References</Heading>
 <Button href='https://www.faa.gov/sites/faa.gov/files/regulations_policies/handbooks_manuals/aviation/airplane_handbook/00_afh_full.pdf#page=181'>Airplane Flying Handbook (FAA-H-8083-3C) Chapter 9</Button>
 <Heading tag='h2'>Completion Standards</Heading>
--- flight-instruction/src/routes/maneuvers/goaround/lesson.md
+++ flight-instruction/src/routes/maneuvers/goaround/lesson.md
@@ -1,8 +1,47 @@
-#Go Arounds
+#Go-Arounds
 
 Learning how to abort a landing is arguably the most important step in learning how to land safely. While it may seem like a simple task, there are many mistakes that pilots can make that can lead to potentially dangerous situations. This lesson will focus on when to do a go around and common mistakes that people make when attempting them.
 
+##When to Execute a Go-Around
 
+A go-around should be executed whenever any approach and landing parameters deviate from expectations or when it is harzardous to continue. A few common reasons you may need to go around are below:
+
+*Avoiding a conflict on a runway (could be other traffic or any obstruction)
+*Not properly aligned with the runway centerline
+*Wind shear on final
+*Being too fast or too slow
+*Being too high or too low
+*When something doesn't feel right
+
+Think about it like this: expect to go around, landing is just a bonus. Your flight instructor will never be mad at you for going around, even if there was nothing wrong with your approach. It's always better to be extra cautious. There is no risk in going around, assuming you know how to properly execute one, but there is a lot of risk in continuing an approach that you don't feel comfortable with.
+
+##Don't Delay When Something is Off
+
+There are many reasons why people continue bad landings, however they often happen from two reasons: landing expectancy and pride.
+
+###Landing Expectancy
+
+The belief that conditions are not as threatening as they are and the approach is sure to terminate with a safe landing. This false assumption can affect all pilots, regardless of their experience level. Remember, always take the safe choice, and never underestimate hazards you are facing.
+
+###Pride
+
+Going around is not a failure, in fact it is a good measure of aeronautical decision making. Maybe you're worried about impressing your DPE, or just a passenger. Trust me, neither of them are going to be impressed with you continuing a bad landing and ripping off one of the landing gear. Turning a bad approach into an even worse landing is never a good idea.
+
+##Executing a Go-Around
+
+A proper go-around includes three principles, in this order:
+
+*Power
+*Attitude
+*Configuration
+
+###Power
+
+This should be your first concern. As soon as you decide to go around, apply full power. You need to overcome the inertia of the airplane's descent and increase your airspeed. Remember that the application of power should be smooth, so don't just slam the throttle forward. 
+
+###Attitude
+
+###Configuration
 
 ##References
 !"https://www.faa.gov/sites/faa.gov/files/regulations_policies/handbooks_manuals/aviation/airplane_handbook/00_afh_full.pdf#page=181"Airplane Flying Handbook (FAA-H-8083-3C) Chapter 9
