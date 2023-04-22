bb9afe0530e4980bc2e96c783cc49453f636156b
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-04-22 10:39:16 -0400

    power off stalls lesson completed

--- flight-instruction/src/routes/maneuvers/stalls/poweroff/+page.svelte
+++ flight-instruction/src/routes/maneuvers/stalls/poweroff/+page.svelte
@@ -11,6 +11,72 @@
 <Heading tag='h1'>Power-Off Stalls</Heading>
 <P>Power-off stalls are normally performed with normal landing approach conditions to simulate an accidental stall during approach to landing. A power-off stall, however, can occur at any airspeed or pitch attitude, and should be comprehensively trained for in a variety of attitudes and flap settings.</P>
 <Blockquote border bg class="p-4 my-4"><P size="xl" height="relaxed">NOTE: Before practicing any type of stall, it is imperative to have a full understanding of angle of attack and load factor. This is not a maneuver that you can practice without the prerequisite knowledge.</P></Blockquote>
+<Heading tag='h2'>Simulated vs Accidental Stalls</Heading>
+<P>All types of stalls are practiced at high altitude to ensure that a pilot has sufficient room to fully regain control of the airplane. However, accidental stalls often occur close to the ground, where there is minimal time and altitude to recover. For this reason, we not only want to train how to recover from a stall, but also how to notice the signs of an impending stall so that we can avoid having to recover in the first place.</P>
+<P>Real life power-off stalls will most likely occur gradually, so that is how we want to train them. There should not be any abrupt control inputs prior to the stall.</P>
+<Heading tag='h2'>The Procedure</Heading>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Select an altitude that will allow for recovery above 1,500 feet AGL and perform pre-maneuver checklist and position report</Li></List>
+<P>This is our recovery altitude, so our initial altitude should be higher. At least 3,000 feet AGL when starting out is a good idea.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Maintain heading, altitude, and airspeed</Li></List>
+<P>Remember to pick a good visual reference point outside. During the stall we will be at a higher than normal pitch attitude, so picking a prominent cloud is a good idea.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Set power to 1500 RPM, adjust pitch to maintain altitude</Li>
+<Li>As airspeed decreases, continue adding flaps until fully extended (if performing in the landing configuration)</Li></List>
+<P>Make sure to add flaps in notches as airspeed decreases. Do not wait until you are slow and then add them all in at the same time.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Slow to approach speed of 65 KIAS and adjust pitch for final approach descent</Li>
+<Li>After descending more than 100 feet, reduce power to idle while increasing angle of attack to maintain altitude</Li></List>
+<P>This part should be gradual. We are not trying to enter into a stall as quickly as possible. Allow the airspeed to bleed off, and pay attention to all of the indications of an impending stall.</P>
+<div><Tabs style='underline'>
+<TabItem open title="Imminent Stall"><div class="contents">
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Observe the first indcation of airflow separation and decrease of control effectiveness</Li>
+<Li>Make "Stall warning" callout</Li>
+<Li>Recover</Li></List>
+</div></TabItem>
+<TabItem title="Full Stall"><div class="contents">
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Observe the first indcation of airflow separation and decrease of control effectiveness</Li>
+<Li>Make "Stall warning" callout</Li>
+<Li>Wait for a sudden decrease in pitch attitude and complete loss of control effectiveness occurs</Li>
+<Li>make "Stall" callout</Li>
+<Li>Recover</Li></List>
+</div></TabItem></Tabs></div>
+<Heading tag='h5'>Recovery</Heading>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Promptly release the backpressure from the flight controls to reduce the angle of attack</Li></List>
+<P>Reducing angle of attack is our first concern in recovery from any stall</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Add full power</Li>
+<Li>Establish Vx pitch to allow for minimum loss of altitude</Li>
+<Li>Retract flaps to 20 degrees</Li></List>
+<Blockquote border bg class="p-4 my-4"><P size="xl" height="relaxed">NOTE: Do not attempt to level the wings using ailerons while stalled, this may cause the aircraft to enter incipient spin.</P></Blockquote>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Passing 60 KIAS, retract flaps to 10 degrees</Li>
+<Li>Set Vy pitch attitude</Li>
+<Li>Passing 70 KIAS, fully retract the flaps</Li>
+<Li>Climb to beginning altitude or an altitude higher (as specified by examiner or instructor)</Li>
+<Li>Resume normal cruise and perform cruise checklist</Li></List>
+<div class="image"><Img size="max-w-full" src=https://i.imgur.com/GDmnQ4E.png></Img></div>
+<Heading tag='h2'>A Few Notes</Heading>
+<Heading tag='h5'>Coordination</Heading>
+<P>Coordination is very important throughout any stall maneuver. Remember that your most effect roll control throughout a stall is going to the the rudder. As you increase power and pitch in the recovery, remember to add the proper amount of right rudder to counteract left turning tendencies.</P>
+<Heading tag='h5'>Banked Stalls</Heading>
+<P>You will also practice power-off stalls in shallow banks. The recovery from these stalls will be very similar to level stalls. Our primary concern is always reducing angle of attack, so remember to focus on that over leveling the wings.</P>
+<Heading tag='h5'>Make The "Stall Warning" and "Stall" Callouts</Heading>
+<P>As we practice intentionally entering into stalls, it is extremely important to always make the proper callouts to acknowledge that we are purposefly entering into a stall. You never want to get used to the indications of a stall, because when one happens inadvertently you might not react properly.</P>
+<Heading tag='h2'>Common Errors</Heading>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Failure to adequately clear the area</Li>
+<Li>Over-reliance on the airspeed indicator and slip-skid indicator while excluding other cues after recovery</Li>
+<Li>Inadvertent accelerated stall by pulling too fast on the controls during a power-off or power-on stall entry</Li>
+<Li>Inability to recognize an impending stall condition</Li>
+<Li>Failure to take timely action to prevent a full stall during the conduct of impending stalls</Li>
+<Li>Failure to maintain a constant bank angle during turning stalls</Li>
+<Li>Failure to maintain proper coordination with the rudder throughout the stall and recovery</Li>
+<Li>Recovering before reaching the critical AOA when practicing the full stall maneuver</Li>
+<Li>Not disconnecting the wing leveler or autopilot, if equipped, prior to reducing AOA</Li>
+<Li>Recovery is attempted without recognizing the importance of pitch control and AOA</Li>
+<Li>Not maintaining a nose down control input until the stall warning is eliminated</Li>
+<Li>Pilot attempts to level the wings before reducing AOA</Li>
+<Li>Pilot attempts to recover with power before reducing AOA</Li>
+<Li>Failure to roll wings level after AOA reduction and stall warning is eliminated</Li>
+<Li>Inadvertent secondary stall during recovery</Li>
+<Li>Excessive forward-elevator pressure during recovery resulting in low or negative G load</Li>
+<Li>Excessive airspeed buildup during recovery</Li>
+<Li>Losing situational awareness and failing to return to desired flightpath or follow ATC instructions</Li></List>
 <Heading tag='h2'>References</Heading>
 <Button href='https://www.faa.gov/sites/faa.gov/files/regulations_policies/handbooks_manuals/aviation/airplane_handbook/00_afh_full.pdf#page=122'>Airplane Flying Handbook (FAA-H-8083-3C) Chapter 5</Button>
 <Heading tag='h2'>Completion Standards</Heading>
--- flight-instruction/src/routes/maneuvers/stalls/poweroff/lesson.md
+++ flight-instruction/src/routes/maneuvers/stalls/poweroff/lesson.md
@@ -5,8 +5,86 @@ Power-off stalls are normally performed with normal landing approach conditions 
 $NOTE: Before practicing any type of stall, it is imperative to have a full understanding of angle of attack and load factor. This is not a maneuver that you can practice without the prerequisite knowledge.
 
 
+##Simulated vs Accidental Stalls
+
+All types of stalls are practiced at high altitude to ensure that a pilot has sufficient room to fully regain control of the airplane. However, accidental stalls often occur close to the ground, where there is minimal time and altitude to recover. For this reason, we not only want to train how to recover from a stall, but also how to notice the signs of an impending stall so that we can avoid having to recover in the first place.
+
+Real life power-off stalls will most likely occur gradually, so that is how we want to train them. There should not be any abrupt control inputs prior to the stall.
+
+##The Procedure
+*Select an altitude that will allow for recovery above 1,500 feet AGL and perform pre-maneuver checklist and position report
+This is our recovery altitude, so our initial altitude should be higher. At least 3,000 feet AGL when starting out is a good idea.
+
+*Maintain heading, altitude, and airspeed
+Remember to pick a good visual reference point outside. During the stall we will be at a higher than normal pitch attitude, so picking a prominent cloud is a good idea.
+
+*Set power to 1500 RPM, adjust pitch to maintain altitude
+*As airspeed decreases, continue adding flaps until fully extended (if performing in the landing configuration)
+Make sure to add flaps in notches as airspeed decreases. Do not wait until you are slow and then add them all in at the same time.
+
+*Slow to approach speed of 65 KIAS and adjust pitch for final approach descent
+*After descending more than 100 feet, reduce power to idle while increasing angle of attack to maintain altitude
+This part should be gradual. We are not trying to enter into a stall as quickly as possible. Allow the airspeed to bleed off, and pay attention to all of the indications of an impending stall.
+
+{tabs}
+{Imminent Stall}
+*Observe the first indcation of airflow separation and decrease of control effectiveness
+*Make "Stall warning" callout
+*Recover
+{Full Stall}
+*Observe the first indcation of airflow separation and decrease of control effectiveness
+*Make "Stall warning" callout
+*Wait for a sudden decrease in pitch attitude and complete loss of control effectiveness occurs
+*make "Stall" callout
+*Recover
+{/tabs}
+
+###Recovery
+*Promptly release the backpressure from the flight controls to reduce the angle of attack
+Reducing angle of attack is our first concern in recovery from any stall
+*Add full power
+*Establish Vx pitch to allow for minimum loss of altitude
+*Retract flaps to 20 degrees
+$NOTE: Do not attempt to level the wings using ailerons while stalled, this may cause the aircraft to enter incipient spin.
+*Passing 60 KIAS, retract flaps to 10 degrees
+*Set Vy pitch attitude
+*Passing 70 KIAS, fully retract the flaps
+*Climb to beginning altitude or an altitude higher (as specified by examiner or instructor)
+*Resume normal cruise and perform cruise checklist
+
+@https://i.imgur.com/GDmnQ4E.png
 
 
+##A Few Notes
+###Coordination
+Coordination is very important throughout any stall maneuver. Remember that your most effect roll control throughout a stall is going to the the rudder. As you increase power and pitch in the recovery, remember to add the proper amount of right rudder to counteract left turning tendencies.
+###Banked Stalls
+You will also practice power-off stalls in shallow banks. The recovery from these stalls will be very similar to level stalls. Our primary concern is always reducing angle of attack, so remember to focus on that over leveling the wings.
+###Make The "Stall Warning" and "Stall" Callouts
+As we practice intentionally entering into stalls, it is extremely important to always make the proper callouts to acknowledge that we are purposefly entering into a stall. You never want to get used to the indications of a stall, because when one happens inadvertently you might not react properly.
+
+
+
+
+##Common Errors
+*Failure to adequately clear the area
+*Over-reliance on the airspeed indicator and slip-skid indicator while excluding other cues after recovery
+*Inadvertent accelerated stall by pulling too fast on the controls during a power-off or power-on stall entry
+*Inability to recognize an impending stall condition
+*Failure to take timely action to prevent a full stall during the conduct of impending stalls
+*Failure to maintain a constant bank angle during turning stalls
+*Failure to maintain proper coordination with the rudder throughout the stall and recovery
+*Recovering before reaching the critical AOA when practicing the full stall maneuver
+*Not disconnecting the wing leveler or autopilot, if equipped, prior to reducing AOA
+*Recovery is attempted without recognizing the importance of pitch control and AOA
+*Not maintaining a nose down control input until the stall warning is eliminated
+*Pilot attempts to level the wings before reducing AOA
+*Pilot attempts to recover with power before reducing AOA
+*Failure to roll wings level after AOA reduction and stall warning is eliminated
+*Inadvertent secondary stall during recovery
+*Excessive forward-elevator pressure during recovery resulting in low or negative G load
+*Excessive airspeed buildup during recovery
+*Losing situational awareness and failing to return to desired flightpath or follow ATC instructions
 
 ##References
 !"https://www.faa.gov/sites/faa.gov/files/regulations_policies/handbooks_manuals/aviation/airplane_handbook/00_afh_full.pdf#page=122"Airplane Flying Handbook (FAA-H-8083-3C) Chapter 5
