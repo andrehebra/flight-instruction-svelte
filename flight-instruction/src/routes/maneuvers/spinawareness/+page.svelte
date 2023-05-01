00851bdd7b20c41b7b818956f40737cd8898821a
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-05-01 15:10:06 -0400

    spin awareness lesson, need to add more info

--- flight-instruction/src/routes/maneuvers/spinawareness/+page.svelte
+++ flight-instruction/src/routes/maneuvers/spinawareness/+page.svelte
@@ -11,10 +11,36 @@
 
 <Heading tag='h1'>Spin Awareness</Heading>
 <P>A spin is an aggravated stall condition that can occur after a stall. Learning the causes of spins is incredibly important, as well as learning proper spin recovery techniques.</P>
+<Blockquote border bg class="p-4 my-4"><P size="sm" height="relaxed">NOTE: Neither private or commercial applicants should practice spins. This lesson is intended to only be a resource for recovering from unintentional spins. Spins should only be intentionally entered into when practicing with a certified instructor.</P></Blockquote>
 <div class="image"><Img size="max-w-full" src=https://i.imgur.com/DRXek28.png></Img></div>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>What Causes Spins?</Heading>
 <P>A spin occurs when at least one of the airplane's wings exceeds the critical angle of attack with a sideslip or yaw acting on the airplane. This yaw can be coming from incorrect rudder application, adverse yaw from aileron deflection, or the natural turning tendencies of the airplane. Therefore, the key to not entering a spin is maintaining directional control and not allowing the nose to yaw before stall recovery is initiated.</P>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Spins vs Spirals</Heading>
+<P>It is important to note that a spiral and a spin are fundamentally different. A spiral, for example the steep spiral maneuver performed by commercial pilot applicants, is characterized by a relatively high airspeed and high descent rate. However, neither wing is close to exceeding the critical angle of attack, and therefore the airplane is under full control. An uncontrolled spiral is still a dangerous maneuver, but not for the same reasons that make a spin dangerous.</P>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Aerodynamics of Spins</Heading>
+<P>Because all spins begin with an uncoordinated stall, we can understand that part of the fuselage is disrupting the airflow over one of the wings. A simple depiction of this is shown below:</P>
+<div class="image"><Img size="max-w-full" src=https://i.imgur.com/0Zn4MqQ.png></Img></div>
+<P>If we assume that the airplane in the figure is in a skidding turn (meaning excessive rudder inputs towards the center of the turn), then the wing that is receiving less airflow is also traveling at a slower speed compared to the outside wing. This can further aggravate over-banking tendencies as well as cause the nose to pitch down, even outside of a stall entry.</P>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Phases of a Spin</Heading>
+<Heading tag='h5'>Entry</Heading>
+<P>This can be either intentional or unintentional. A typical intentional spin entry would be characterized by performing a stall while purposely adding excessive rudder in one direction.</P>
+<Heading tag='h5'>Incipient Phase</Heading>
+<P>This phase refers to the period from a stall to a full spin. As the spin is not fully developed, the aerodynamic forces are not balanced. The airplane will continue rolling towards the stalled wing and rapidly accelerate downwards. Rotation will also increase.</P>
+<Heading tag='h5'>Developed Phase</Heading>
+<P>Both wings are stalled, however the inside wing is at a higher angle of attack. The spin will stabilize with the airplane at a low airspeed and stable descent and rotation rate. The descent rate will be very high.</P>
+<Heading tag='h5'>Recovery</Heading>
+<P>The steps to recover from a spin can be remembered by using the acronym PARE:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Power: Idle</Li>
+<Li>Ailerons: Neutral</Li>
+<Li>Rudder: Opposite direction of the spin</Li>
+<Li>Elevator: Forward</Li></List>
+<P>Opposite to traditional stall recovery, power is reduced to aid in lowering the angle of attack. Aerodynamically, this is because adding power increases pitch and subsequently angle of attack. Keeping the ailerons neutral helps the wings reach the same angle of attack by decreasing the pitching and rolling moments. Rudder in the opposite direction of the spin counteracts the rotation, and pushing elevator forward further reduces angle of attack.</P>
+<P>It is imperative that you have these steps memorized. A spin can quickly develop and subsequently cause you to lose thousands of feet of altitude in a short time. There is no time in this situation to be worrying about remembering an acronym or pulling out a checklist.</P>
+<div class="image"><Img size="max-w-full" src=https://i.imgur.com/kR1BQYR.png></Img></div>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Completion Standards</Heading>
 <P>Spin awareness is included in both the Private and Commercial ACS. The requirements are the same for both.</P>
--- flight-instruction/src/routes/maneuvers/spinawareness/lesson.md
+++ flight-instruction/src/routes/maneuvers/spinawareness/lesson.md
@@ -2,6 +2,8 @@
 
 A spin is an aggravated stall condition that can occur after a stall. Learning the causes of spins is incredibly important, as well as learning proper spin recovery techniques.
 
+$NOTE: Neither private or commercial applicants should practice spins. This lesson is intended to only be a resource for recovering from unintentional spins. Spins should only be intentionally entered into when practicing with a certified instructor.
+
 @https://i.imgur.com/DRXek28.png
 
 --
@@ -9,6 +11,52 @@ A spin is an aggravated stall condition that can occur after a stall. Learning t
 ##What Causes Spins?
 
 A spin occurs when at least one of the airplane's wings exceeds the critical angle of attack with a sideslip or yaw acting on the airplane. This yaw can be coming from incorrect rudder application, adverse yaw from aileron deflection, or the natural turning tendencies of the airplane. Therefore, the key to not entering a spin is maintaining directional control and not allowing the nose to yaw before stall recovery is initiated.
+
+--
+
+##Spins vs Spirals
+
+It is important to note that a spiral and a spin are fundamentally different. A spiral, for example the steep spiral maneuver performed by commercial pilot applicants, is characterized by a relatively high airspeed and high descent rate. However, neither wing is close to exceeding the critical angle of attack, and therefore the airplane is under full control. An uncontrolled spiral is still a dangerous maneuver, but not for the same reasons that make a spin dangerous.
+
+--
+
+##Aerodynamics of Spins
+
+Because all spins begin with an uncoordinated stall, we can understand that part of the fuselage is disrupting the airflow over one of the wings. A simple depiction of this is shown below:
+
+@https://i.imgur.com/0Zn4MqQ.png
+
+If we assume that the airplane in the figure is in a skidding turn (meaning excessive rudder inputs towards the center of the turn), then the wing that is receiving less airflow is also traveling at a slower speed compared to the outside wing. This can further aggravate over-banking tendencies as well as cause the nose to pitch down, even outside of a stall entry.
+
+--
+
+##Phases of a Spin
+
+###Entry
+
+This can be either intentional or unintentional. A typical intentional spin entry would be characterized by performing a stall while purposely adding excessive rudder in one direction.
+
+###Incipient Phase
+
+This phase refers to the period from a stall to a full spin. As the spin is not fully developed, the aerodynamic forces are not balanced. The airplane will continue rolling towards the stalled wing and rapidly accelerate downwards. Rotation will also increase.
+
+###Developed Phase
+
+Both wings are stalled, however the inside wing is at a higher angle of attack. The spin will stabilize with the airplane at a low airspeed and stable descent and rotation rate. The descent rate will be very high.
+
+###Recovery
+
+The steps to recover from a spin can be remembered by using the acronym PARE:
+*Power: Idle
+*Ailerons: Neutral
+*Rudder: Opposite direction of the spin
+*Elevator: Forward
+
+Opposite to traditional stall recovery, power is reduced to aid in lowering the angle of attack. Aerodynamically, this is because adding power increases pitch and subsequently angle of attack. Keeping the ailerons neutral helps the wings reach the same angle of attack by decreasing the pitching and rolling moments. Rudder in the opposite direction of the spin counteracts the rotation, and pushing elevator forward further reduces angle of attack.
+
+It is imperative that you have these steps memorized. A spin can quickly develop and subsequently cause you to lose thousands of feet of altitude in a short time. There is no time in this situation to be worrying about remembering an acronym or pulling out a checklist.
+
+@https://i.imgur.com/kR1BQYR.png
 
 --
 
