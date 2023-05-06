e217b70cae8f74dbd947a1ee4b15fde6f3c010ff
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-05-06 10:00:36 -0400

    removed enc1102

--- flight-instruction/src/routes/enc1102/instagrampost/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import NavBar from "../../components/NavBar.svelte";
-    import FirestoreLessonConstructor from '../../components/FirestoreLessonConstructor.svelte';
-
-    let contents = [['image', 'https://i.imgur.com/7A8Fp66.png'],];
-</script>
-
-<FirestoreLessonConstructor contents={contents}></FirestoreLessonConstructor>
