dbb5af33dcb86e6feb1abdd0869bc08f244b4b61
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-06-19 15:19:12 -0400

    working on learning process

--- flight-instruction/src/routes/lessons/foi/learningprocess/+page.svelte
+++ flight-instruction/src/routes/lessons/foi/learningprocess/+page.svelte
@@ -9,6 +9,90 @@
 
 <div class="holder"><div class="contents">
 
+<Heading tag='h1'>The Learning Process</Heading>
+<P>Learning can be defined in many different ways, but generally we are talking about:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>A change in behavior of the learner as a result of experience</Li>
+<Li>The process of experience which brings about a relatively permanent change in behavior</Li>
+<Li>gaining knowledge, skills, or developing a behavior through study, instruction, or experience</Li></List>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Learning Theory</Heading>
+<P>The explanation of how people acquire skills, knowledge, and attitudes. There are two accepted concepts of how people learn: Behaviorism and Cognitive Theory.</P>
+<Heading tag='h5'>Behaviorism</Heading>
+<P>Explains animal and human behavior in terms of observable and measurable responses to stimuli. Can be generally thought of as a system of rewards and punishment which causes people to learn. This system stresses the importance of positively rewarding behavior that an instructor deems acceptable in the learner.</P>
+<P>Behaviorism has been losing some popularity more recently because learning can be described as a much more complex process compared to simply rewards and punishments.</P>
+<Heading tag='h5'>Cognitive</Heading>
+<P>In this theory, we focus on what is going on in the mind of a learner, considering:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Knowing</Li>
+<Li>Perceiving</Li>
+<Li>Problem Solving</Li>
+<Li>Decision-Making</Li>
+<Li>Awareness</Li>
+<Li>Etc.</Li></List>
+<Heading tag='h5'>Concepts in Cognitive Theory:</Heading>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Reflective Thought</Li></List>
+<P>One main focus of cognitive theory is reflection. Learning improves because a learner is able to reflect on their own performance. This is useful to the degree that a learner is able to understand their own mistakes.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Assimilation and Accommodation</Li></List>
+<P>Tension between old ideas meeting new situations and changing old ideas to meet the new situations, or showing that the old ideas hold up to new situations.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Spiral Curriculum</Li></List>
+<P>Revisiting basic ideas repeatedly and building on them in more sophisticated ways will cause students to mature and develop</P>
+<Heading tag='h5'>Blooms Taxonomy of the Cognitive Domain</Heading>
+<P>comprises levels of intellectual behavior and progress from the simplest to the most complex</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Knowledge</Li></List>
+<P>Being able to recollect the information which was learned.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Comprehension</Li></List>
+<P>Demonstrate comprehension through one or more forms of explanation.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Application</Li></List>
+<P>Use information or a skill in a new situation.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Analysis</Li></List>
+<P>Break material into its constituent parts and determine how the parts relate to one another and/or to an overall structure or purpose.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Synthesis and Evaluation</Li></List>
+<P>Make judgments based on criteria and standards and evaluate one's own or other's performance.</P>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Perceptions and Insight</Heading>
+<P>All learning initially comes from perceptions, which are directed to the brain by one or more of the five senses.</P>
+<Heading tag='h5'>Factors that affect perception:</Heading>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Physical Organism</Li>
+<Li>Goals and Values</Li>
+<Li>Self-concept</Li>
+<Li>Time and Opportunity</Li>
+<Li>Element of Threat</Li></List>
+<Heading tag='h5'>Insight</Heading>
+<P>The grouping of perceptions into meaningful wholes. It is the instructor's responsibility to ensure that insights occur during instruction.</P>
+<P>Insights will occur whether or not instruction is taking place, the difference is that the instructor can dramatically increase the speed at which the insights are created.</P>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>The Laws of Learning</Heading>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Readiness</Li></List>
+<P>The basic needs of the learner need to be satisfied before he or she is ready or capable of learning. The instructor can not do much to motivate a learner if those needs have not been met.</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Effect</Li></List>
+<P>The law of effect states that behaviors that lead to satisfying outcomes are likely to be repeated whereas behaviors that lead to undesired outcomes are less likely to recur. The learner needs to have success in order to have more success in the future. The instructor needs to create situations designed to promote success by using positive training experiences</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Exercise</Li></List>
+<P>Connections are strengthened with practice and weakened when practice is discontinued. Essentially, "use it or lose it"</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Primacy</Li></List>
+<P>What is learned first often creates a strong and almost unshakable impression and underlies the reason an instructor needs to teach correctly the first time</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Intensity</Li></List>
+<P>Exciting or dramatic learning connected to a real situation teaches a learner more than a routine or boring experience</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Recency</Li></List>
+<P>The most recently learned thing is best remembered</P>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Domains of Learning</Heading>
+<P>Cognitive (knowledge), Affective (attitude), and Psychomotor (skills).</P>
+<div class="image"><Img size="max-w-full" src=https://i.imgur.com/aDln6CD.png></Img></div>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Characteristics of Learning</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Acquiring Skill Knowledge</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Types of Practice</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Scenario-Based Training</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Errors</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Memory and Forgetting</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Retention of Learning</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Transfer of Learning</Heading>
 </div></div>
 
 
---
+++ flight-instruction/src/routes/lessons/foi/learningprocess/lesson.md
@@ -0,0 +1,171 @@
+#The Learning Process
+
+Learning can be defined in many different ways, but generally we are talking about:
+
+*A change in behavior of the learner as a result of experience
+*The process of experience which brings about a relatively permanent change in behavior
+*gaining knowledge, skills, or developing a behavior through study, instruction, or experience
+
+--
+
+##Learning Theory
+
+The explanation of how people acquire skills, knowledge, and attitudes. There are two accepted concepts of how people learn: Behaviorism and Cognitive Theory.
+
+###Behaviorism
+Explains animal and human behavior in terms of observable and measurable responses to stimuli. Can be generally thought of as a system of rewards and punishment which causes people to learn. This system stresses the importance of positively rewarding behavior that an instructor deems acceptable in the learner.
+
+Behaviorism has been losing some popularity more recently because learning can be described as a much more complex process compared to simply rewards and punishments.
+
+###Cognitive
+In this theory, we focus on what is going on in the mind of a learner, considering:
+
+*Knowing
+*Perceiving
+*Problem Solving
+*Decision-Making
+*Awareness
+*Etc.
+
+
+
+###Concepts in Cognitive Theory:
+
+*Reflective Thought
+One main focus of cognitive theory is reflection. Learning improves because a learner is able to reflect on their own performance. This is useful to the degree that a learner is able to understand their own mistakes.
+
+*Assimilation and Accommodation
+Tension between old ideas meeting new situations and changing old ideas to meet the new situations, or showing that the old ideas hold up to new situations.
+
+*Spiral Curriculum
+Revisiting basic ideas repeatedly and building on them in more sophisticated ways will cause students to mature and develop
+
+
+###Blooms Taxonomy of the Cognitive Domain
+
+comprises levels of intellectual behavior and progress from the simplest to the most complex
+
+*Knowledge
+Being able to recollect the information which was learned.
+*Comprehension
+Demonstrate comprehension through one or more forms of explanation.
+*Application
+Use information or a skill in a new situation.
+*Analysis
+Break material into its constituent parts and determine how the parts relate to one another and/or to an overall structure or purpose.
+*Synthesis and Evaluation
+Make judgments based on criteria and standards and evaluate one's own or other's performance.
+
+
+
+
+--
+
+##Perceptions and Insight
+
+All learning initially comes from perceptions, which are directed to the brain by one or more of the five senses.
+
+###Factors that affect perception:
+
+*Physical Organism
+*Goals and Values
+*Self-concept
+*Time and Opportunity
+*Element of Threat
+
+###Insight
+
+The grouping of perceptions into meaningful wholes. It is the instructor's responsibility to ensure that insights occur during instruction.
+
+Insights will occur whether or not instruction is taking place, the difference is that the instructor can dramatically increase the speed at which the insights are created.
+
+--
+
+
+##The Laws of Learning
+
+*Readiness
+The basic needs of the learner need to be satisfied before he or she is ready or capable of learning. The instructor can not do much to motivate a learner if those needs have not been met.
+
+*Effect
+The law of effect states that behaviors that lead to satisfying outcomes are likely to be repeated whereas behaviors that lead to undesired outcomes are less likely to recur. The learner needs to have success in order to have more success in the future. The instructor needs to create situations designed to promote success by using positive training experiences
+
+*Exercise
+Connections are strengthened with practice and weakened when practice is discontinued. Essentially, "use it or lose it"
+
+*Primacy
+What is learned first often creates a strong and almost unshakable impression and underlies the reason an instructor needs to teach correctly the first time
+
+*Intensity
+Exciting or dramatic learning connected to a real situation teaches a learner more than a routine or boring experience
+
+*Recency
+The most recently learned thing is best remembered
+
+
+
+--
+
+##Domains of Learning
+
+Cognitive (knowledge), Affective (attitude), and Psychomotor (skills).
+
+@https://i.imgur.com/aDln6CD.png
+
+###Cognitive Domain
+
+@
+
+
+
+--
+
+##Characteristics of Learning
+
+
+--
+
+##Acquiring Skill Knowledge
+
+
+
+--
+
+##Types of Practice
+
+
+--
+
+##Scenario-Based Training
+
+
+
+--
+
+##Errors
+
+
+
+--
+
+##Memory and Forgetting
+
+
+
+--
+
+##Retention of Learning
+
+
+
+--
+
+##Transfer of Learning
+
+
+
+
+
+
+
+
