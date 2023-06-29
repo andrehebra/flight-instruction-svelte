f472bd8876e3707c5546c5b4392f92ccee8b660a
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-06-29 14:57:03 -0400

    outline of teaching process complete

--- flight-instruction/src/routes/lessons/foi/teachingprocess/+page.svelte
+++ flight-instruction/src/routes/lessons/foi/teachingprocess/+page.svelte
@@ -9,6 +9,21 @@
 
 <div class="holder"><div class="contents">
 
+<Heading tag='h1'>The Teaching Process</Heading>
+<Heading tag='h2'>Preparation of a Lesson</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Organization of Material</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Training Delivery Methods</Heading>
+<Heading tag='h5'>Lecture Method</Heading>
+<Heading tag='h5'>Guided Discussion Method</Heading>
+<Heading tag='h5'>Computer-assisted Method</Heading>
+<Heading tag='h5'>Demonstration-Performance Method</Heading>
+<Heading tag='h5'>Drill and Practice Method</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Problem Based Learning</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Instruction Aids and Training Technologies</Heading>
 </div></div>
 
 
---
+++ flight-instruction/src/routes/lessons/foi/teachingprocess/lesson.md
@@ -0,0 +1,50 @@
+#The Teaching Process
+
+##Preparation of a Lesson
+
+
+
+--
+##Organization of Material
+
+
+
+--
+##Training Delivery Methods
+
+
+###Lecture Method
+
+
+###Guided Discussion Method
+
+
+###Computer-assisted Method
+
+
+###Demonstration-Performance Method
+
+
+###Drill and Practice Method
+
+
+
+
+--
+##Problem Based Learning
+
+
+
+--
+##Instruction Aids and Training Technologies
+
+
+
+
+
+
+
+
+
+
+
