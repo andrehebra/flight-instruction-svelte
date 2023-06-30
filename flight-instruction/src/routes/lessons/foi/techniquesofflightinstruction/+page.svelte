89b014ed5196ddd83d86e668da93e46ff49cdba8
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-06-29 20:08:51 -0400

    outline of techniques of flight instruction

--- flight-instruction/src/routes/lessons/foi/techniquesofflightinstruction/+page.svelte
+++ flight-instruction/src/routes/lessons/foi/techniquesofflightinstruction/+page.svelte
@@ -9,6 +9,23 @@
 
 <div class="holder"><div class="contents">
 
+<Heading tag='h1'>Techniques of Flight Instruction</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Obstacles in Learning During Flight Instruction</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Demonstration-Performance Training Delivery</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Positive Exchange of Controls</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Sterile Cockpit</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Use of Distractions</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Integrated Flight Instruction</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Assessment of Piloting Ability</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Aeronautical Decision Making</Heading>
 </div></div>
 
 
---
+++ flight-instruction/src/routes/lessons/foi/techniquesofflightinstruction/lesson.md
@@ -0,0 +1,45 @@
+#Techniques of Flight Instruction
+
+
+--
+##Obstacles in Learning During Flight Instruction
+
+
+
+--
+##Demonstration-Performance Training Delivery
+
+
+
+--
+##Positive Exchange of Controls
+
+
+
+--
+##Sterile Cockpit
+
+
+
+--
+##Use of Distractions
+
+
+
+--
+##Integrated Flight Instruction
+
+
+
+--
+##Assessment of Piloting Ability
+
+
+
+--
+##Aeronautical Decision Making
+
+
+
+
+
