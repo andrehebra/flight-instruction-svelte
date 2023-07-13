3d8c39845ce4d164e3d3d819698830216254a9c9
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-07-13 15:10:26 -0400

    working on runway incursion avoidance

--- flight-instruction/src/routes/lessons/technicalsubjects/incursionavoidance/+page.svelte
+++ flight-instruction/src/routes/lessons/technicalsubjects/incursionavoidance/+page.svelte
@@ -9,6 +9,70 @@
 
 <div class="holder"><div class="contents">
 
+<Heading tag='h1'>Runway Incursion Avoidance</Heading>
+<P>A runway incursion is any occurrence at an airport involving an aircraft, vehicle, person, or object on the ground that creates a collision hazard or results in a loss of separation with an aircraft taking off, landing, or intending to land.</P>
+<P>Proper planning can prevent runway incursions and the possibility of ground collisions.</P>
+<P>There are about 3 runway incursions each day at towered airports in the United States, of which 65% are caused by pilots (according to the FAA).</P>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Contributing Factors to Runway Incursions</Heading>
+<P>There are three main contributing factors that lead to runway incursions:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Communications</Li>
+<Li>Airport knowledge</Li>
+<Li>Flight deck procedures for maintaining orientation</Li></List>
+<Heading tag='h5'>Common Deviations that Lead to Runway Incursions</Heading>
+<div><Tabs style='underline'>
+<TabItem open title="Pilot Deviations"><div class="contents">
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Crossing a runway hold marking without a clearance</Li>
+<Li>Taking off without a clearance</Li>
+<Li>Landing without a clearance</Li></List>
+</div></TabItem>
+<TabItem title="Operational Incidents"><div class="contents">
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Clearing an aircraft onto a runway while another aircraft is landing on the same runway</Li>
+<Li>Issuing a takeoff clearance while the runway is occupied by another aircraft or vehicle</Li></List>
+</div></TabItem>
+<TabItem title="Vehicle Deviations"><div class="contents">
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Crossing a runway hold marking without a clearance</Li></List>
+</div></TabItem></Tabs></div>
+<Heading tag='h5'>Causal Factors of Runway Incursions</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Sterile Cockpit</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Taxiing Safely</Heading>
+<Heading tag='h5'>Towered Airports</Heading>
+<Heading tag='h5'>Non-towered Airports</Heading>
+<Heading tag='h5'>Crossing Runways</Heading>
+<Heading tag='h5'>ForeFlight Taxi Routing</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Hold Short Instructions and Markings</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Line Up and Wait (LUAW) Operations</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Runway Shortened Operations</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Runway Confusion</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Exiting the Runway After Landing</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Engineered Materials Arresting Systems</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Airport Markings and Signs</Heading>
+<Heading tag='h5'>Runway Markings</Heading>
+<Heading tag='h5'>Taxiway Markings</Heading>
+<Heading tag='h5'>Hold Position Markings</Heading>
+<Heading tag='h5'>Other Markings</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>ILS Critical Area Purpose and Markings</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Airport Signs</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Airport Lighting</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Airport Hot Spots</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Night Operations</Heading>
+<Hr class="my-8" height="h-px" />
+<Heading tag='h2'>Low Visibility Operations</Heading>
+<Hr class="my-8" height="h-px" />
 </div></div>
 
 
---
+++ flight-instruction/src/routes/lessons/technicalsubjects/incursionavoidance/lesson.md
@@ -0,0 +1,117 @@
+#Runway Incursion Avoidance
+
+A runway incursion is any occurrence at an airport involving an aircraft, vehicle, person, or object on the ground that creates a collision hazard or results in a loss of separation with an aircraft taking off, landing, or intending to land.
+
+Proper planning can prevent runway incursions and the possibility of ground collisions.
+
+There are about 3 runway incursions each day at towered airports in the United States, of which 65% are caused by pilots (according to the FAA).
+
+
+--
+##Contributing Factors to Runway Incursions
+
+There are three main contributing factors that lead to runway incursions:
+*Communications
+*Airport knowledge
+*Flight deck procedures for maintaining orientation
+
+###Common Deviations that Lead to Runway Incursions
+
+{tabs}
+{Pilot Deviations}
+*Crossing a runway hold marking without a clearance
+*Taking off without a clearance
+*Landing without a clearance
+
+{Operational Incidents}
+*Clearing an aircraft onto a runway while another aircraft is landing on the same runway
+*Issuing a takeoff clearance while the runway is occupied by another aircraft or vehicle
+
+{Vehicle Deviations}
+*Crossing a runway hold marking without a clearance
+
+{/tabs}
+
+###Causal Factors of Runway Incursions
+
+--
+##Sterile Cockpit
+
+
+--
+##Taxiing Safely
+
+###Towered Airports
+
+###Non-towered Airports
+
+###Crossing Runways
+
+###ForeFlight Taxi Routing
+
+
+--
+##Hold Short Instructions and Markings
+
+
+--
+##Line Up and Wait (LUAW) Operations
+
+
+--
+##Runway Shortened Operations
+
+
+--
+##Runway Confusion
+
+
+--
+##Exiting the Runway After Landing
+
+
+--
+##Engineered Materials Arresting Systems
+
+
+--
+##Airport Markings and Signs
+
+###Runway Markings
+
+###Taxiway Markings
+
+###Hold Position Markings
+
+###Other Markings
+
+--
+##ILS Critical Area Purpose and Markings
+
+
+--
+##Airport Signs
+
+
+--
+##Airport Lighting
+
+
+--
+##Airport Hot Spots
+
+
+--
+##Night Operations
+
+
+--
+##Low Visibility Operations
+
+
+--
+
+
+
+
+
