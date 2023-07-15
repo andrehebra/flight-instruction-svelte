5919c3fe183eb166efe76abfd8429869cb0ac6c0
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-07-14 22:29:44 -0400

    finished visual scanning and collision avoidance

--- flight-instruction/src/routes/lessons/technicalsubjects/scanningandcollisionavoidance/+page.svelte
+++ flight-instruction/src/routes/lessons/technicalsubjects/scanningandcollisionavoidance/+page.svelte
@@ -10,24 +10,83 @@
 <div class="holder"><div class="contents">
 
 <Heading tag='h1'>Visual Scanning and Collision Avoidance</Heading>
+<P>The majority of aircraft collisions happen during the daytime in clear weather. Oftentimes this gives pilots a sense of security, but it is incredibly important that pilots maintain vigilance in all different conditions.</P>
+<P>Risk factors for a collision include:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Below 3,000 feet AGL</Li>
+<Li>Within 5 miles of an airport</Li>
+<Li>During daytime clear weather</Li>
+<Li>Aircraft blind spots (low and high wing)</Li></List>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Vision Related to Current Physical Condition</Heading>
+<P>Nobody has perfect vision, but there are things that can affect a person's ability to see well.</P>
+<P>Things that have an impact on a pilot's vision:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Diet</Li>
+<Li>Physical Health</Li>
+<Li>Illness</Li>
+<Li>Medication</Li>
+<Li>Stress</Li>
+<Li>Alcohol</Li>
+<Li>Fatigue</Li>
+<Li>Emotion</Li>
+<Li>Hypoxia</Li></List>
+<Blockquote border bg class="p-4 my-4"><P size="sm" height="relaxed">Did you see a hidden checklist in there? I wonder what it could be?</P></Blockquote>
+<P>Remember that almost anything that has an effect on your physical or mental condition can degrade your vision.</P>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Environmental Conditions that Degrade Vision</Heading>
+<P>There are many different environmental conditions that can degrade you ability to see well.</P>
+<Heading tag='h5'>Darkness</Heading>
+<P>It can take 30 minutes for your eyes to adjust to darkness. Unfortunately, however, you can quickly lose your night vision by looking at a bright light for a few seconds.</P>
+<P>Tips for maintaining night vision:</P>
+<List ulClass="max-w" tag="ul" class="space-y-1"><Li>Close one eye when you need to look at something bright to maintain your night vision</Li>
+<Li>Dim cabin lights and displays</Li>
+<Li>Use red lights as to not interfere with night vision</Li></List>
+<Heading tag='h5'>High Altitude</Heading>
+<P>Flying at higher altitudes can impair your vision, especially at night.</P>
+<P>Besides what the legal requirements are, it is recommended that pilots use supplemental oxygen at pressure altitudes above 10,000 feet MSL during the daytime and 5,000 MSL at night.</P>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Vestibular and Visual Illusions</Heading>
+<Button href='/lessons/technicalsubjects/aeromedical/opticalillusions'>Optical Illusions</Button>
+<Button href='/lessons/technicalsubjects/aeromedical/spatialdisorientation'>Spatial Disorientation and Vestibular Illusions</Button>
+<Heading tag='h5'>Visual Blind Spot</Heading>
+<P>The area where the optic nerve connects to the retina in the back of each eye is known as the optic disk. In that area, there is a total absence of cones and rods, and therefore you are not able to see anything in that spot.</P>
+<P>Focus on the airplane in the image below. Cover or close your left eye and move towards the image. You will eventually notice where the X on the right side disappears.</P>
+<div class="image"><Img size="max-w-full" src="https://imagedelivery.net/bfLWD0Qjtd593vL0tnvGqQ/c776c235-40a4-4e5e-8e61-917b22c7df00/public"></Img></div>
+<P>Your blind spot is normally subconsciously tuned out by your brain, but you need to consider it as a risk in being able to avoid other traffic.</P>
 <Hr class="my-8" height="h-px" />
-<Heading tag='h2'>"See and Avoid"</Heading>
+<Heading tag='h2'>See and Avoid</Heading>
+<P>All pilot's must maintain vigilance at all times while operating an aircraft, both on the ground and in the air.</P>
+<P>Don't ever let ATC give you a false sense of security when it comes to other traffic. There is always the potential of a collision, and you can dramatically reduce that risk by staying focused.</P>
+<P>If you see other traffic, regardless of whether or not you have the right of way, make sure to stay clear of it. It is always possible that other traffic does not see you.</P>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Proper Visual Scanning</Heading>
+<P>Pilot's need to continually scan for traffic throughout their flight.</P>
+<P>A good scan will go from one side to the other, and stop in increments of about 10 degrees. Focus for a second or two on that specific area, and then move on to the next area.</P>
+<P>It can be extremely difficult to notice other traffic when you are not making a conscious effort or focusing in a specific area.</P>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Proper Clearing Procedures</Heading>
+<P>You should always make sure that an area is clear of traffic before you enter it. Here are a few tips for different operations:</P>
+<Heading tag='h5'>Before Takeoff</Heading>
+<P>Angle the airplane towards the approach path when holding short of a runway before takeoff. This will allow you to check if traffic is on final approach before you take the runway.</P>
+<P>Scan the entire approach area and runway to make sure there is no potential for a collision.</P>
+<Heading tag='h5'>Climbs and Descents</Heading>
+<P>Use gentle banks from one side to another to allow you to scan above or below the airplane.</P>
+<Heading tag='h5'>Straight and Level</Heading>
+<P>Constantly scan for traffic with proper visual scanning technique.</P>
+<Heading tag='h5'>Before Maneuvers</Heading>
+<P>Before any maneuver, make sure to execute clearing turns. This should include either two 90 degree turns or a 180 degree turn.</P>
+<P>Always clear in the direction that you will be doing the maneuver in. For example, before a steep turn to the left, turn 90 degrees to the left and 90 degrees back to make sure you will not interfere with any other traffic.</P>
+<Heading tag='h5'>Traffic Patterns</Heading>
+<P>Always scan for traffic when entering the traffic pattern and when inside.</P>
+<P>Before turning base or final, lift the wingtip (in a high wing airplane) and look for traffic on final.</P>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Airplane Blind Spots</Heading>
+<P>High wing airplanes have blind spots above them, while low wing airplanes have blind spots below due to the wings.</P>
+<P>Before turns in a high wing, lift the wingtip and look for traffic in the direction of the turn.</P>
+<P>Before turns in a low wing, lower the wingtip and look for traffic in the direction of the turn.</P>
 <Hr class="my-8" height="h-px" />
 <Heading tag='h2'>Speed Differential and Collision Risk</Heading>
-<Hr class="my-8" height="h-px" />
-<Heading tag='h2'>Most Common Risk Scenarios</Heading>
+<P>An increased speed differential will result in pilot's having less time to react and avoid a collision.</P>
+<P>Additionally, head on collisions can be difficult to notice as it will not appear as though the aircraft is moving along the windshield.</P>
 <Hr class="my-8" height="h-px" />
 </div></div>
 
--- flight-instruction/src/routes/lessons/technicalsubjects/scanningandcollisionavoidance/lesson.md
+++ flight-instruction/src/routes/lessons/technicalsubjects/scanningandcollisionavoidance/lesson.md
@@ -1,41 +1,130 @@
 #Visual Scanning and Collision Avoidance
 
+The majority of aircraft collisions happen during the daytime in clear weather. Oftentimes this gives pilots a sense of security, but it is incredibly important that pilots maintain vigilance in all different conditions.
+
+Risk factors for a collision include:
+*Below 3,000 feet AGL
+*Within 5 miles of an airport
+*During daytime clear weather
+*Aircraft blind spots (low and high wing)
+
+
+
 --
 ##Vision Related to Current Physical Condition
 
+Nobody has perfect vision, but there are things that can affect a person's ability to see well.
+
+Things that have an impact on a pilot's vision:
+*Diet
+*Physical Health
+*Illness
+*Medication
+*Stress
+*Alcohol
+*Fatigue
+*Emotion
+*Hypoxia
+
+$Did you see a hidden checklist in there? I wonder what it could be?
+
+Remember that almost anything that has an effect on your physical or mental condition can degrade your vision.
 
 --
 ##Environmental Conditions that Degrade Vision
+There are many different environmental conditions that can degrade you ability to see well.
+
+###Darkness
+
+It can take 30 minutes for your eyes to adjust to darkness. Unfortunately, however, you can quickly lose your night vision by looking at a bright light for a few seconds.
+
+Tips for maintaining night vision:
+*Close one eye when you need to look at something bright to maintain your night vision
+*Dim cabin lights and displays
+*Use red lights as to not interfere with night vision
+
+###High Altitude
+Flying at higher altitudes can impair your vision, especially at night.
+
+Besides what the legal requirements are, it is recommended that pilots use supplemental oxygen at pressure altitudes above 10,000 feet MSL during the daytime and 5,000 MSL at night.
 
 
 --
 ##Vestibular and Visual Illusions
 
+!"/lessons/technicalsubjects/aeromedical/opticalillusions"Optical Illusions
+!"/lessons/technicalsubjects/aeromedical/spatialdisorientation"Spatial Disorientation and Vestibular Illusions
+
+###Visual Blind Spot
+
+The area where the optic nerve connects to the retina in the back of each eye is known as the optic disk. In that area, there is a total absence of cones and rods, and therefore you are not able to see anything in that spot.
+
+Focus on the airplane in the image below. Cover or close your left eye and move towards the image. You will eventually notice where the X on the right side disappears.
+
+&c776c235-40a4-4e5e-8e61-917b22c7df00
+
+Your blind spot is normally subconsciously tuned out by your brain, but you need to consider it as a risk in being able to avoid other traffic.
 
 --
-##"See and Avoid"
+##See and Avoid
 
+All pilot's must maintain vigilance at all times while operating an aircraft, both on the ground and in the air.
+
+Don't ever let ATC give you a false sense of security when it comes to other traffic. There is always the potential of a collision, and you can dramatically reduce that risk by staying focused.
+
+If you see other traffic, regardless of whether or not you have the right of way, make sure to stay clear of it. It is always possible that other traffic does not see you.
 
 --
 ##Proper Visual Scanning
 
+Pilot's need to continually scan for traffic throughout their flight.
+
+A good scan will go from one side to the other, and stop in increments of about 10 degrees. Focus for a second or two on that specific area, and then move on to the next area.
+
+It can be extremely difficult to notice other traffic when you are not making a conscious effort or focusing in a specific area.
 
 --
 ##Proper Clearing Procedures
 
+You should always make sure that an area is clear of traffic before you enter it. Here are a few tips for different operations:
+
+###Before Takeoff
+Angle the airplane towards the approach path when holding short of a runway before takeoff. This will allow you to check if traffic is on final approach before you take the runway.
+Scan the entire approach area and runway to make sure there is no potential for a collision.
+
+###Climbs and Descents
+Use gentle banks from one side to another to allow you to scan above or below the airplane.
+
+###Straight and Level
+Constantly scan for traffic with proper visual scanning technique.
+
+###Before Maneuvers
+Before any maneuver, make sure to execute clearing turns. This should include either two 90 degree turns or a 180 degree turn.
+
+Always clear in the direction that you will be doing the maneuver in. For example, before a steep turn to the left, turn 90 degrees to the left and 90 degrees back to make sure you will not interfere with any other traffic.
+
+###Traffic Patterns
+Always scan for traffic when entering the traffic pattern and when inside.
+
+Before turning base or final, lift the wingtip (in a high wing airplane) and look for traffic on final.
 
 --
 ##Airplane Blind Spots
 
+High wing airplanes have blind spots above them, while low wing airplanes have blind spots below due to the wings.
+
+Before turns in a high wing, lift the wingtip and look for traffic in the direction of the turn.
+
+Before turns in a low wing, lower the wingtip and look for traffic in the direction of the turn.
 
 --
 ##Speed Differential and Collision Risk
 
+An increased speed differential will result in pilot's having less time to react and avoid a collision.
+
+Additionally, head on collisions can be difficult to notice as it will not appear as though the aircraft is moving along the windshield.
 
 --
-##Most Common Risk Scenarios
 
-
---
 
 
